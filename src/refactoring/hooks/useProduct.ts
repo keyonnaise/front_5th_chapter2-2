6c8adffab2127153c787fc349d6f911e8a3f3a0e
keyonnaise <su.smisc14@gmail.com>
2025-04-20 22:50:31 +0900

    ✨ useProducts 훅에 관리 기능 구현

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState(initialProducts);
+
+  const addProduct = useCallback((product: Product) => {
+    setProducts((prev) => [...prev, product]);
+  }, []);
+
+  const updateProduct = useCallback((product: Product) => {
+    setProducts((prev) => prev.map((current) => (current.id === product.id ? product : current)));
+  }, []);
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    addProduct,
+    updateProduct,
   };
 };
