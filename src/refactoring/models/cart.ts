a298804e343f8f9d8eb331946cc993378e81c5c4
keyonnaise <su.smisc14@gmail.com>
2025-04-21 01:59:46 +0900

    ✨ 장바구니 계산 기능 및 쿠폰 적용 로직 추가

--- src/refactoring/models/cart.ts
+++ src/refactoring/models/cart.ts
@@ -1,22 +1,52 @@
 import { CartItem, Coupon } from "../../types";
 
 export const calculateItemTotal = (item: CartItem) => {
-  return 0;
+  const discountRate = getMaxApplicableDiscount(item);
+  const totalBeforeDiscount = item.product.price * item.quantity;
+  const totalAfterDiscount = totalBeforeDiscount * (1 - discountRate);
+  return totalAfterDiscount;
 };
 
 export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+  const maxRate = item.product.discounts.reduce((maxRate, discount) => {
+    return maxRate < discount.rate && discount.quantity <= item.quantity ? discount.rate : maxRate;
+  }, 0);
+  return maxRate;
 };
 
-export const calculateCartTotal = (
-  cart: CartItem[],
-  selectedCoupon: Coupon | null
-) => {
-  return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
-  };
+export const calculateCartTotal = (cart: CartItem[], selectedCoupon: Coupon | null) => {
+  const result = cart.reduce(
+    (result, item) => {
+      const totalBeforeDiscount = result.totalBeforeDiscount + item.product.price * item.quantity;
+      const totalAfterDiscount = result.totalAfterDiscount + calculateItemTotal(item);
+      const totalDiscount = totalBeforeDiscount - totalAfterDiscount;
+
+      return {
+        totalBeforeDiscount,
+        totalAfterDiscount,
+        totalDiscount,
+      };
+    },
+    {
+      totalBeforeDiscount: 0,
+      totalAfterDiscount: 0,
+      totalDiscount: 0,
+    }
+  );
+
+  // 선택된 쿠폰이 있다면 쿠폰을 적용합니다.
+  if (selectedCoupon !== null) {
+    const appliedCoupon = applyCoupon(result.totalAfterDiscount, selectedCoupon);
+    const totalDiscount = result.totalBeforeDiscount - appliedCoupon;
+
+    return {
+      ...result,
+      totalDiscount,
+      totalAfterDiscount: appliedCoupon,
+    };
+  }
+
+  return result;
 };
 
 export const updateCartItemQuantity = (
@@ -24,5 +54,22 @@ export const updateCartItemQuantity = (
   productId: string,
   newQuantity: number
 ): CartItem[] => {
-  return [];
+  // 수량이 0으로 설정된 경우 항목을 제거
+  if (newQuantity === 0) {
+    return cart.filter((item) => item.product.id !== productId);
+  }
+
+  const updated = cart.map((item) => {
+    return item.product.id === productId
+      ? { ...item, quantity: Math.min(newQuantity, item.product.stock) }
+      : item;
+  });
+
+  return updated;
+};
+
+const applyCoupon = (amount: number, coupon: Coupon) => {
+  return coupon.discountType === "amount"
+    ? amount - coupon.discountValue
+    : amount - amount / coupon.discountValue;
 };
